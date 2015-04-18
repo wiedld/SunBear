e0a61c998106a043f1c5be89c404d509d3619539
wiedld <dlw405@gmail.com>
2015-04-18 15:01:42 -0700

    persistent db built for census data

--- model.py
+++ model.py
@@ -16,11 +16,83 @@ Session = None
 # function connect() at bottom...before main.
 ## in python shell:  -i model.py > s=connect() >
 
-class neighborhood_demo(Base):
-    __tablename__ = "Neighborhood"
+class Demographic(Base):
+    """from Social Explorer"""
+    __tablename__ = "Demographics"
+    id = Column(Integer, primary_key=True)
+    zipcode = Column(String(15))    # is unique per row
+    popdensity = Column(Float)
+    pctemployed = Column(Float)
+    pctmnf = Column(Float)
+    pctlogistics = Column(Float)
+    pctit = Column(Float)
+    pctprof = Column(Float)
+    hhincq10 = Column(Integer)
+    hhincq30 = Column(Integer)
+    hhincq50 = Column(Integer)
+    hhincq70 = Column(Integer)
+    hhincq90 = Column(Integer)
+    hhincq95 = Column(Integer)
+    pctheatelec = Column(Float)
+
+
+# object attributes:
+# zip,popdensity,pctemployed,pctmnf,pctlogistics,pctit,pctprof,hhincq10,hhincq30,hhincq50,hhincq70,hhincq90,hhincq95,pctheatelec
+
+# object attributes - detailed name:
+# zip,Population Density (per sq. mile),% of labor force employed,% employed in manufacturing,% empl logistics,% empl  IT,% empl prof,Households: Quintile Means: Lowest Quintile,Households: Quintile Means: Second Quintile,Households: Quintile Means: Third Quintile,Households: Quintile Means: Fourth Quintile,Households: Quintile Means: Highest Quintile,Households: Quintile Means: Top 5 Percent,% heat with elect
 
 
 
+class Geographic(Base):
+    """from Social Explorer"""
+    __tablename__ = "Geographics"
+    id = Column(Integer, primary_key=True)
+    zipcode = Column(String(15))    # is unique per row
+    type_addy = Column(String(100))
+    primary_city = Column(String(30))
+    acceptable_cities = Column(String(100))
+    unacceptable_cities = Column(String(100))
+    state = Column(String(2))
+    county = Column(String(30))
+    timezone = Column(String(30))
+    area_codes = Column(String(3))
+    latitude = Column(Float)
+    longitude = Column(Float)
+    world_region = Column(String(30))
+    country = Column(String(2))           # select only for US
+    decommissioned = Column(String(10))
+    estimated_population = Column(Integer)
+    notes = Column(String(100))
+
+
+# object attributes
+# zip,type,primary_city,acceptable_cities,unacceptable_cities,state,county,timezone,area_codes,latitude,longitude,world_region,country,decommissioned,estimated_population,notes
+
+
+
+
+
+class zillow_demo(Base):
+    """from zillow API"""
+    __tablename__ = "Zillow_demos"
+    id = Column(Integer, primary_key=True)
+    zipcode = Column(String(15))    # is unique per row
+    type_addy = Column(String(100))
+    primary_city = Column(String(30))
+    acceptable_cities = Column(String(100))
+    unacceptable_cities = Column(String(100))
+    state = Column(String(2))
+    county = Column(String(30))
+    timezone = Column(String(30))
+    area_codes = Column(String(3))
+    latitude = Column(Float)
+    longitude = Column(Float)
+    world_region = Column(String(30))
+    country = Column(String(2))           # select only for US
+    decommissioned = Column(String(10))
+    estimated_population = Column(Integer)
+    notes = Column(String(100))
 
 ######################################################################
 
@@ -29,8 +101,10 @@ def connect():
     global Session
 
     ENGINE = create_engine("sqlite:///DataBear.db", echo=True)
-    Session = scoped_session(sessionmaker(bind=ENGINE, autocommit=False, autoflush=False))
-    Base.query = Session.query_property()
+    Session = sessionmaker(bind=ENGINE)
+
+    # Session = scoped_session(sessionmaker(bind=ENGINE, autocommit=False, autoflush=False))
+    # Base.query = Session.query_property()
     return Session()
 
     # urlparse.uses_netloc.append("postgres")
--- seeding.py
+++ seeding.py
@@ -2,3 +2,88 @@ import model
 import csv
 from datetime import datetime
 
+
+
+def load_geographics():
+    with open('seed_data/zip_code_database.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, dialect='excel')
+        for row in reader:
+            print ("row:", row)
+            # row = troubleshooting.  If issue, see where.
+            try:
+                session=model.connect()
+                geo_obj = model.Geographic()
+
+                geo_obj.zipcode = row[0]
+                geo_obj.type_addy = row[1]
+                geo_obj.primary_city = row[2]
+                geo_obj.acceptable_cities = row[3]
+                geo_obj.unacceptable_cities = row[4]
+                geo_obj.state = row[5]
+                geo_obj.county = row[6]
+                geo_obj.timezone = row[7]
+                geo_obj.area_codes = row[8]
+                geo_obj.latitude = float(row[9])
+                geo_obj.longitude = float(row[10])
+                geo_obj.world_region = row[11]
+                geo_obj.country = row[12]
+                geo_obj.decommissioned = row[13]
+                geo_obj.estimated_population = int(row[14])
+                geo_obj.notes = row[15]
+
+                session.add(geo_obj)
+                session.commit()
+            except:
+                print "Error for row data:", row
+                f = open('log_file.txt','a')
+                f.write("\nError.  CAISO_daily_api, Demand data, failure for row:"+str(row))
+                f.close
+
+
+
+
+def load_demographics():
+    with open('seed_data/SlimmerData_Consolidated.csv', 'rU') as csvfile:
+        reader = csv.reader(csvfile, dialect='excel')
+        for row in reader:
+            print "row:", row
+            # row = troubleshooting.  If issue, see where.
+            try:
+                session=model.connect()
+                demo_obj = model.Demographic()
+
+                demo_obj.zipcode = row[0]
+                demo_obj.popdensity = float(row[1])
+                demo_obj.pctemployed = float(row[2])
+                demo_obj.pctmnf = float(row[3])
+                demo_obj.pctlogistics = float(row[4])
+                demo_obj.pctit = float(row[5])
+                demo_obj.pctprof = float(row[6])
+                demo_obj.hhincq10 = int(row[7])
+                demo_obj.hhincq30 = int(row[8])
+                demo_obj.hhincq50 = int(row[9])
+                demo_obj.hhincq70 = int(row[10])
+                demo_obj.hhincq90 = int(row[11])
+                demo_obj.hhincq95 = int(row[12])
+                demo_obj.pctheatelec = float(row[13])
+
+                session.add(demo_obj)
+                session.commit()
+            except:
+                print "Error for row data:", row
+                f = open('log_file.txt','a')
+                f.write("\nError. Failure for row:"+str(row))
+                f.close
+
+
+
+def main():
+    """this contains all the different functions for seeding.  Comment out when seeding is complete"""
+    # load_geographics()
+    # load_demographics()
+
+
+
+
+if __name__ == "__main__":
+    main()
